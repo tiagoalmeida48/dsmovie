382699a75d0a999fae9aa3d243f3fd138f37d54c
Tiago Almeida <tiagoalmeida48@yahoo.com.br>
2022-01-12 23:02:52 -0300

    Navbar

---
+++ frontend/src/components/navbar/index.tsx
@@ -0,0 +1,21 @@
+import {ReactComponent as GithubIcon} from 'assets/img/github.svg';
+
+function Navbar() {
+  return(
+    <header>
+    <nav className="container">
+      <div className="dsmovie-nav-content">
+        <h1>DSMovie</h1>
+        <a href="https://github.com/tiagoalmeida48">
+          <div className="dsmovie-contact-content">
+            <GithubIcon />
+            <p className="dsmovie-contact-link">/devsuperior</p>
+          </div>
+        </a>
+      </div>
+    </nav>
+  </header> 
+  )  
+}
+
+export default Navbar
